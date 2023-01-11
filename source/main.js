d6db229c8d25f774f21ea542d73c4984773cdc9b
khanhnhanbk <nhanhoa21012002@gmail.com>
2023-01-11 17:51:50 +0700

    edit output

--- source/main.js
+++ source/main.js
@@ -8,7 +8,7 @@ const myWalletAddress = myKey.getPublic('hex');
 const anotherKey = ec.keyFromPrivate('e1f2c51debc92cfad2fffd9196e10e000a5160da417bfbce36d5cee6edc9b350');   
 const anotherWalletAddress = anotherKey.getPublic('hex');
 
-let myCoin = new Blockchain(2);
+let myCoin = new Blockchain(3);
 
 const tx1 = new Transaction(myWalletAddress, anotherWalletAddress, 10);
 tx1.signTransaction(myKey);
@@ -16,7 +16,8 @@ myCoin.addTransaction(tx1);
 
 console.log('\n Starting the miner...');
 myCoin.miningPendingTransactions(myWalletAddress);
-console.log('\n Balance of xavier is', myCoin.checkBalance(myWalletAddress));
+console.log('\n Balance of me is', myCoin.checkBalance(myWalletAddress));
+console.log('\n Balance of another', myCoin.checkBalance(anotherWalletAddress));
 
 console.log('\n Starting the miner again...');
 myCoin.miningPendingTransactions(myWalletAddress);
